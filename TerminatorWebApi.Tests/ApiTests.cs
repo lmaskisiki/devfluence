9b95a581b1c28988d7fcc7093dcda905dfe518f3
Lizo <lizom@sahomeloans.com>
2018-04-26 09:10:46 +0200

    added learning test for members to investigate nancy testing further

---
+++ TerminatorWebApi.Tests/ApiTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+
+namespace TerminatorWebApi.Tests
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        [Test]
+        public void Health_WhenCalled_ShouldReturnStatusCodeOK()
+        {
+            // Arrange
+            var bootstraper= new DefaultNancyBootstrapper();
+            var browser = new Browser(bootstraper);
+ 
+            // Act
+            var result = browser.Get("/health", with =>
+            {
+                with.HttpRequest();
+            });
+
+            // Assert
+         ///    Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            var content = result.ContentType;
+        }
+
+        // var browser = new Browser(with => { with.Module<TerminatorApiModule>(); });
+
+    }
+}
---
+++ TerminatorWebApi/TerminatorApiModule.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+
+namespace TerminatorWebApi
+{
+    public class TerminatorApiModule : NancyModule
+    {
+        public TerminatorApiModule()
+        {
+            Get["/health"] = _ => Negotiate
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithModel(new {Name="Lizo",Age=12});
+        }
+    }
+}

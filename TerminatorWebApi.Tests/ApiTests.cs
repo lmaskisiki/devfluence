c7f40496ab617b38f57ed6f86f4322325aa6144a
Sachin Moodley <sachinmoodley12@gmail.com>
2018-04-26 11:56:35 +0200

    added health endpoint

---
+++ TerminatorWebApi.Tests/ApiTests.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+
+namespace TerminatorWebApi.Tests
+{
+    [TestFixture]
+    class ApiTests
+    {
+        [Test]
+        public void Health_WhenCalled_ShouldReturnStatusCodeOK()
+        {
+            // Arrange 
+            var browser = new Browser(with => with.Module(new HealthApi()));
+
+            // Act
+            var result = browser.Get("/health");
+
+            // Assert       
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}

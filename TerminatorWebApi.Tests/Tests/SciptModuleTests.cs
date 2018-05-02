e2396d9afe7f185da7fc842f00ec70e9b29518f8
Sachin Moodley <sachinmoodley12@gmail.com>
2018-05-02 10:37:05 +0200

    test

--- TerminatorWebApi.Tests/Tests/SciptModuleTests.cs
+++ TerminatorWebApi.Tests/Tests/SciptModuleTests.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using MachineInformationApp;
 using MachineInformationApp.Interfaces;
 using Nancy;
 using Nancy.Testing;
-using Newtonsoft.Json;
 using NSubstitute;
 using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;

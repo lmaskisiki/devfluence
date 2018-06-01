dccaf564d2252db27896222c33a5f24fb8d15214
Sachin Moodley <sachinmoodley12@gmail.com>
2018-06-01 13:27:16 +0200

    added script folder

---
+++ TerminatorWebApi.Tests/sqlScripts/Create Dashboard.sql
@@ -0,0 +1,23 @@
+﻿USE [SkyNetPortalDB]
+GO
+
+/****** Object: Table [dbo].[Dashboard] Script Date: 2018/05/31 15:58:46 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+Drop Table [dbo].[Dashboard]
+CREATE TABLE [dbo].[Dashboard] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [Target]        NVARCHAR (50) NULL,
+    [Action]       NVARCHAR (50) NULL,
+    [ActionResult]  NVARCHAR (50) NULL,
+    [ExecutionTime] DATETIME      NULL
+);
+
+Insert into [dbo].[Dashboard] ([Target],[Action],[ActionResult], [ExecutionTime]) Values('Dashboard','Add Agent','Added Agent 1', '2018-12-31 23:59:59');
+
+select * from [dbo].[Dashboard]
+
+
---
+++ TerminatorWebApi.Tests/sqlScripts/create.sql
@@ -0,0 +1,19 @@
+﻿
+drop table [dbo].[AgentExecution]
+ CREATE TABLE [dbo].[AgentExecution]
+(
+	[ExecutionId] INT NOT NULL IDENTITY(1,1),  
+    [Command] NVARCHAR(50) NULL, 
+    [Result] NVARCHAR(50) NULL, 
+    [ExecutionTime] DATETIME NULL
+	
+)
+
+insert into AgentExecution
+(Command,Result,ExecutionTime) Values('ip','193.192.2.194','2018-12-31 23:59:59');
+
+insert into AgentExecution
+(Command,Result,ExecutionTime) Values('hostname','DevFluence7','2018-01-01 21:59:59');
+ 
+
+select * FROM dbo.AgentExecution;
